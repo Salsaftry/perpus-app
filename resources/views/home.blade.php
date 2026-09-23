1a5c91ea2bd42602828f30922f6cd7060447ad50
unknown <salsafitrya04@gmail.com>
2026-09-23 13:58:02 +0700

    Pertemuan 3-routing,controller,blade dasar

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ $judul }}</title>
+</head>
+<body>
+    <h1> Selamat Datang di {{ $judul }}</h1>
+    <p>Ini adalah halaman utama aplikasi yang sedang di bangun pada mata kuliah Pemrograman Web II.</p>
+</body>
+</html>
---
+++ resources/views/kontak.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('title', 'Kontak')
+
+@section('content')
+    <h1>Kontak</h1>
+    <p>Aplikasi ini dibuat sebagai studi kasus pembelajaran mata kuliah Pemrograman Web II, berupa Sistem Informasi peminjaman buku perpustakaan mini.</p>
+@endsection
